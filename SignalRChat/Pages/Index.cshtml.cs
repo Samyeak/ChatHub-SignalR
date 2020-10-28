9d7c32a90bd3d37ae15137b2fe55da18dc444626
Samyeak <10978827+Samyeak@users.noreply.github.com>
2020-10-28 09:13:23 +0545

    Add project files.

---
+++ SignalRChat/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace SignalRChat.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHubContext<ChatHub> _chatHub;
+
+        public IndexModel(ILogger<IndexModel> logger, IHubContext<ChatHub> chatHub)
+        {
+            _logger = logger;
+            _chatHub = chatHub;
+        }
+
+        public async void OnGet()
+        {
+            await _chatHub.Clients.All.SendAsync("ReceiveMessage", "One", "Hello from Index Page");
+        }
+    }
+}
